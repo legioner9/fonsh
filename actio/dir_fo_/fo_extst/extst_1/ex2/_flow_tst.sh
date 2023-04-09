48454be557ca63f3e8f9f726616f57f1d168032d
[alt]legioner9 <legioner9@inbox.ru>
2023-04-09 20:51:10 +0700

    <> first fn #2

---
+++ actio/dir_fo_/fo_extst/extst_1/ex2/_flow_tst.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#. "$HOME/.bashrc"
+
+filename=${FONSH_PATH}/actio/dir_fo_/fo_extst/extst_1/ex2/_flow_tst.sh 
+
+# echo -e "${HLIGHT}---start file://$filename ---${NORMAL}" # start file
+
+idir=$(pwd)
+
+cd "$(prs_f -d $filename)" || qq_exit "$(prs_f -d filename) not found"
+
+>res
+
+cr_tst_dir_ < dat &>> res
+
+cd "$idir"
+
+unset filename
